ed0e7cbe3e5167ffba8003a24dd25e5c7db14d65
Kiran-naregal <kirnaregal7@gmail.com>
2024-12-15 18:48:06 +0530

    cabin's list and details of individual cabin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "dbxzunbcprablosrzyot.supabase.co",
+                port: "",
+                pathname: "/storage/v1/object/public/cabin-images/**",
+            },
+            {
+                protocol: "https",
+                hostname: "dclaevazetcjjkrzczpc.supabase.co",
+                port: "",
+                pathname: "/storage/v1/object/public/cabin-images/**",
+            },
+        ]
+    },
+    // output: "export",
+};
 
 export default nextConfig;
